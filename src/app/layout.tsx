dcfa900874db6f996c2d988538e6f789a7aae701
matheusc1 <cardoso.matheusbs@gmail.com>
2023-11-16 17:48:00 -0300

    style: create initial layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='w-full h-screen bg-slate-950 flex items-center justify-center'>
+          <main className='flex flex-col items-center justify-start w-96 h-2/3 bg-slate-50 bg-opacity-10 rounded-2xl shadow-md'>
+            {children}
+          </main>
+        </div>
+      </body>
     </html>
   )
 }
