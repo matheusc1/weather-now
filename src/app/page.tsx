8f75c9846f02f2634a4312b6b789843a1664f94b
matheusc1 <cardoso.matheusbs@gmail.com>
2023-12-26 16:02:40 -0300

    feat: implement data fetching function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,38 +2,58 @@
 import WeatherInfo from "@/components/WeatherInfo"
 import { Weather } from "@/types/Weather.interface"
 import { Search } from "lucide-react"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Home() {
+  const [weather, setWeather] = useState<Weather | null>(null)
+  const [search, setSearch] = useState('')
 
-  const [weather, setWeather] = useState<Weather | null>({
-    city: 'Seul',
-    country: 'South Korea',
-    temperature: 1,
-    details: 'clear',
-    realFeel: 2,
-    humidity: 52,
-    windSpeed: 12,
-    pressure: 1016
-  })
+  let notFound = false;
 
-  // const [weather, setWeather] = useState<Weather | null>(null)
-  
-  const notFound = false;
+  async function getWeatherInfo(cityName: String) {
+    setWeather(null)
 
-  // console.log(weather)
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${process.env.NEXT_PUBLIC_WEATHER_API}`
+
+      const response = await fetch(url)
+      const data = await response.json()
+
+      setWeather({
+        city: data.name,
+        country: data.sys.country,
+        temperature: parseFloat((data.main.temp - 273.15).toFixed(1)),
+        details: data.weather[0].main,
+        realFeel: parseFloat((data.main.feels_like - 273.15).toFixed(1)),
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        pressure: data.main.pressure
+      })
+
+      notFound = false;
+    } catch (err) {
+      console.error(err)
+      notFound = true;
+    }
+  }
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setSearch(e.target.value)
+  }
 
   return (
     <>
-      <form className="flex gap-2 mt-4">
+      <div className="flex gap-2 mt-4">
         <input
+          value={search}
+          onChange={handleChange}
           placeholder="Insira o nome da cidade"
           type="text"
           className="border-0 text-slate-200 rounded-lg p-3 bg-slate-600 placeholder:text-slate-300 focus:outline-0" />
-        <button className="bg-slate-600 px-4 rounded-lg hover:bg-slate-500 transition-colors">
+        <button onClick={() => getWeatherInfo(search)} className="bg-slate-600 px-4 rounded-lg hover:bg-slate-500 transition-colors">
           <Search className="text-slate-200" />
         </button>
-      </form>
+      </div>
       
       <section className="mt-7 text-slate-50 text-center">
         {notFound ? (
